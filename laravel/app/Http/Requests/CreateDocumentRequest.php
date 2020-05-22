8eb370829b646f55f53245e4bb98214adcffbab3
zukars3 <59807969+zukars3@users.noreply.github.com>
2020-05-22 18:47:06 +0300

    Adding document validation

---
+++ laravel/app/Http/Requests/CreateDocumentRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateDocumentRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'document' => ['required', 'mimes:pdf', 'max:12000']
+        ];
+    }
+}
