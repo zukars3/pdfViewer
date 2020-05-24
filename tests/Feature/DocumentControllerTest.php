8167d63ec5808c53216b701083f11c6af745436d
zukars3 <59807969+zukars3@users.noreply.github.com>
2020-05-24 21:59:12 +0300

    Adding some tests

---
+++ tests/Feature/DocumentControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DocumentControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
