d73bf9e3421bc0248b855e924ae764d0b47ed140
zukars3 <59807969+zukars3@users.noreply.github.com>
2020-05-25 09:58:51 +0300

    Moving file deletion functionality to Document model

--- app/Document.php
+++ app/Document.php
@@ -3,8 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Document extends Model
 {
     protected $fillable = ['name', 'path', 'thumbnail'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function($document) {
+            Storage::disk('public')->delete([
+                substr($document->path, 8),
+                substr($document->thumbnail, 8),
+            ]);
+        });
+    }
 }
